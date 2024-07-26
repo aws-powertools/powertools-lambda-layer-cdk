e289156fd7a8f86a12c9f6bf47b90afeb92861ec
Leandro Damascena <lcdama@amazon.pt>
2024-07-26 14:58:45 +0100

    Adding Python V3 CDK Layer

--- src/lambda-powertools-layer.ts
+++ src/lambda-powertools-layer.ts
@@ -32,6 +32,11 @@ export interface PowertoolsLayerProps {
    * The compatible architectures for the layer
    */
   readonly compatibleArchitectures?: lambda.Architecture[];
+
+  /**
+   * The Python version for Powertools for AWS Lambda (Python) V3.
+  */
+  readonly pythonVersion?: string;
 }
 
 /**
@@ -109,6 +114,84 @@ export class LambdaPowertoolsLayer extends lambda.LayerVersion {
       compatibleRuntimes: getRuntimesFromRuntimeFamily(runtimeFamily),
       description:
         `Powertools for AWS Lambda (${languageName}) [${compatibleArchitecturesDescription}]${props?.includeExtras ? ' with extra dependencies' : ''
+        } ${props?.version ? `version ${props?.version}` : 'latest version'
+        }`.trim(),
+      // Dear reader: I'm happy that you've stumbled upon this line too! You might wonder, why are we doing this and passing `undefined` when the list is empty?
+      // Answer: on regions that don't support ARM64 Lambdas, we can't use the `compatibleArchitectures` parameter. Otherwise CloudFormation will bail with an error.
+      // So if this construct is called with en empty list of architectures, just pass undefined down to the CDK construct.
+      compatibleArchitectures:
+        compatibleArchitectures.length == 0
+          ? undefined
+          : compatibleArchitectures,
+    });
+  }
+}
+
+export class LambdaPowertoolsLayerPythonV3 extends lambda.LayerVersion {
+  /** CDK Layer for Python v3 **/
+  static constructBuildArgs(
+    includeExtras: boolean | undefined,
+    version: string | undefined,
+  ): string {
+    let suffix = '';
+    if (includeExtras) {
+      suffix = '[all]';
+    }
+    if (version) {
+      if (version.startsWith('git')) {
+        suffix = `${suffix} @ ${version}`;
+      } else {
+        suffix = `${suffix}==${version}`;
+      }
+    }
+    return suffix;
+  }
+
+  constructor(scope: Construct, id: string, props?: PowertoolsLayerProps) {
+    const languageName = 'Python';
+    const dockerFilePath = path.join(__dirname, `../layer/${languageName}v3`);
+    let runtimeFamilyPerVersion;
+    const compatibleArchitectures = props?.compatibleArchitectures ?? [
+      lambda.Architecture.X86_64,
+    ];
+    const compatibleArchitecturesDescription = compatibleArchitectures
+      .map((arch) => arch.name)
+      .join(', ');
+
+    if (props?.pythonVersion == '3.8') {
+      runtimeFamilyPerVersion = lambda.Runtime.PYTHON_3_8;
+    } else if (props?.pythonVersion == '3.9') {
+      runtimeFamilyPerVersion = lambda.Runtime.PYTHON_3_9;
+    } else if (props?.pythonVersion == '3.10') {
+      runtimeFamilyPerVersion = lambda.Runtime.PYTHON_3_10;
+    } else if (props?.pythonVersion == '3.11') {
+      runtimeFamilyPerVersion = lambda.Runtime.PYTHON_3_11;
+    } else if (props?.pythonVersion == '3.12') {
+      runtimeFamilyPerVersion = lambda.Runtime.PYTHON_3_12;
+    } else {
+      runtimeFamilyPerVersion = lambda.Runtime.PYTHON_3_12;
+    }
+
+    console.log(`path ${dockerFilePath}`);
+    super(scope, id, {
+      code: lambda.Code.fromDockerBuild(dockerFilePath, {
+        buildArgs: {
+          PACKAGE_SUFFIX: LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+            props?.includeExtras,
+            props?.version,
+          ),
+          PYTHON_VERSION: props?.pythonVersion ?? '3.12',
+        },
+        // supports cross-platform docker build
+        platform: getDockerPlatformNameFromArchitectures(
+          compatibleArchitectures,
+        ),
+      }),
+      layerVersionName: props?.layerVersionName || undefined,
+      license: 'MIT-0',
+      compatibleRuntimes: [runtimeFamilyPerVersion],
+      description:
+        `Powertools for AWS Lambda (Python) V3 [${compatibleArchitecturesDescription}]${props?.includeExtras ? ' with extra dependencies' : ''
         } ${props?.version ? `version ${props?.version}` : 'latest version'
         }`.trim(),
       // Dear reader: I'm happy that you've stumbled upon this line too! You might wonder, why are we doing this and passing `undefined` when the list is empty?
---
+++ test/lambda-powertools-python-layer-v3.test.ts
@@ -0,0 +1,247 @@
+import { Stack } from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { Architecture, RuntimeFamily } from 'aws-cdk-lib/aws-lambda';
+import { LambdaPowertoolsLayerPythonV3 } from '../src';
+
+describe('with no configuration the construct', () => {
+  const stack = new Stack();
+  new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3');
+  const template = Template.fromStack(stack);
+  test('synthesizes successfully', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      Description: 'Powertools for AWS Lambda (Python) V3 [x86_64] latest version',
+    });
+  });
+
+  test('has license set to MIT-0', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      LicenseInfo: 'MIT-0',
+    });
+  });
+
+  test('matches the default python 3.12 runtimes', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      CompatibleRuntimes: [
+        'python3.12',
+      ],
+    });
+  });
+});
+
+describe('with python 3.8 version', () => {
+  const stack = new Stack();
+  new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+    runtimeFamily: RuntimeFamily.PYTHON,
+    compatibleArchitectures: [Architecture.X86_64],
+    pythonVersion: '3.8',
+  });
+  const template = Template.fromStack(stack);
+  test('matches the default python 3.8 runtimes', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      CompatibleRuntimes: [
+        'python3.8',
+      ],
+    });
+  });
+});
+
+describe('with python 3.9 version', () => {
+  const stack = new Stack();
+  new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+    runtimeFamily: RuntimeFamily.PYTHON,
+    compatibleArchitectures: [Architecture.X86_64],
+    pythonVersion: '3.9',
+  });
+  const template = Template.fromStack(stack);
+  test('matches the default python 3.9 runtimes', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      CompatibleRuntimes: [
+        'python3.9',
+      ],
+    });
+  });
+});
+
+describe('with python 3.10 version', () => {
+  const stack = new Stack();
+  new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+    runtimeFamily: RuntimeFamily.PYTHON,
+    compatibleArchitectures: [Architecture.X86_64],
+    pythonVersion: '3.10',
+  });
+  const template = Template.fromStack(stack);
+  test('matches the default python 3.10 runtimes', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      CompatibleRuntimes: [
+        'python3.10',
+      ],
+    });
+  });
+});
+
+
+describe('with python 3.11 version', () => {
+  const stack = new Stack();
+  new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+    runtimeFamily: RuntimeFamily.PYTHON,
+    compatibleArchitectures: [Architecture.X86_64],
+    pythonVersion: '3.11',
+  });
+  const template = Template.fromStack(stack);
+  test('matches the default python 3.11 runtimes', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      CompatibleRuntimes: [
+        'python3.11',
+      ],
+    });
+  });
+});
+
+describe('with arm64 architecture', () => {
+  const stack = new Stack();
+  new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+    runtimeFamily: RuntimeFamily.PYTHON,
+    compatibleArchitectures: [Architecture.ARM_64],
+  });
+  const template = Template.fromStack(stack);
+  test('synthesizes successfully', () => {
+    template.hasResourceProperties('AWS::Lambda::LayerVersion', {
+      Description: 'Powertools for AWS Lambda (Python) V3 [arm64] latest version',
+      CompatibleArchitectures: ['arm64'],
+    });
+  });
+});
+
+describe('for layerVersionName configuraiton the construct', () => {
+  test('synthisizes to a layer with provided name', () => {
+    const stack = new Stack();
+    new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+      layerVersionName: 'mySpecialName',
+    });
+
+    Template.fromStack(stack).hasResourceProperties(
+      'AWS::Lambda::LayerVersion',
+      {
+        LayerName: 'mySpecialName',
+      },
+    );
+  });
+});
+
+describe('with version configuration the construct', () => {
+  test('synthesizes to a layer with specific valid version', () => {
+    const stack = new Stack();
+    new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerPythonV3', {
+      version: '1.21.0',
+    });
+
+    Template.fromStack(stack).hasResourceProperties(
+      'AWS::Lambda::LayerVersion',
+      {
+        Description:
+          'Powertools for AWS Lambda (Python) V3 [x86_64] version 1.21.0',
+      },
+    );
+  });
+
+  test('fails with invalid version', () => {
+    const stack = new Stack();
+    expect(
+      () =>
+        new LambdaPowertoolsLayerPythonV3(stack, 'PowertoolsLayerBadVersion', {
+          version: '0.0.0',
+        }),
+    ).toThrow(/docker exited with status 1/);
+  });
+
+  test('synthesizes with pydantic and specific version', () => {
+    const stack = new Stack();
+    new LambdaPowertoolsLayerPythonV3(stack, 'LayerExtrasVersion', {
+      includeExtras: true,
+      version: '2.40.0',
+    });
+
+    Template.fromStack(stack).hasResourceProperties(
+      'AWS::Lambda::LayerVersion',
+      {
+        Description:
+          'Powertools for AWS Lambda (Python) V3 [x86_64] with extra dependencies version 2.40.0',
+      },
+    );
+  });
+
+  test('synthesizes with extras and latest version', () => {
+    const stack = new Stack();
+    new LambdaPowertoolsLayerPythonV3(stack, 'LayerExtrasNoVersion', {
+      includeExtras: true,
+    });
+
+    Template.fromStack(stack).hasResourceProperties(
+      'AWS::Lambda::LayerVersion',
+      {
+        Description:
+          'Powertools for AWS Lambda (Python) V3 [x86_64] with extra dependencies latest version',
+      },
+    );
+  });
+});
+
+describe('construct build args for Dockerfile', () => {
+  test('returns extras and version', () => {
+    const args = LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+      true,
+      '1.21.0',
+    );
+
+    expect(args).toEqual('[all]==1.21.0');
+  });
+
+  test('returns only extras when no version provided', () => {
+    const args = LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+      true,
+      undefined,
+    );
+
+    expect(args).toEqual('[all]');
+  });
+
+  test('returns a git url with extras when a git url is provided', () => {
+    const version =
+      'git+https://github.com/awslabs/aws-lambda-powertools-python@v2';
+    const args = LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+      true,
+      version,
+    );
+
+    expect(args).toEqual(`[all] @ ${version}`);
+  });
+
+  test('returns only version when no extras flag provided', () => {
+    const args = LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+      undefined,
+      '1.11.0',
+    );
+
+    expect(args).toEqual('==1.11.0');
+  });
+
+  test('returns empty when no version and extras provided', () => {
+    const args = LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+      undefined,
+      undefined,
+    );
+
+    expect(args).toEqual('');
+  });
+
+  test('returns a git url when a git url is provided and extras provided', () => {
+    const version =
+      'git+https://github.com/awslabs/aws-lambda-powertools-python@v2';
+    const args = LambdaPowertoolsLayerPythonV3.constructBuildArgs(
+      false,
+      version,
+    );
+
+    expect(args).toEqual(` @ ${version}`);
+  });
+});
